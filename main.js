191ced0b75330878707eb4c45d305b1f772aafd3
Anurag-git04 <shawanurag18@gmail.com>
2023-10-06 10:13:32 +0530

    meme generator

---
+++ main.js
@@ -0,0 +1,22 @@
+const generatememebtn=document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage=document.querySelector(".meme-generator img");
+const memeTitle=document.querySelector(".meme-generator .meme-title");
+const memeAuthor=document.querySelector(".meme-generator .meme-author");
+
+const updateDetail=(url, title, author)=>{
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML=title;
+    memeAuthor.innerHTML=`Meme By: ${author}`;
+}
+
+const generateMeme = ()=>{
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then(data =>{
+        updateDetail(data.url, data.title, data.author)
+    })
+}
+
+generatememebtn.addEventListener("click",generateMeme);
+
+generateMeme();
